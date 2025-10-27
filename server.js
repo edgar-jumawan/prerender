dacec024d24f25ead40ebaa6171b77bf606f58d1
Edgar Jumawan <eti.edgar.jumawan@gmail.com>
2025-10-26 21:44:13 -0600

    adding the new package.json and server.js

--- server.js
+++ server.js
@@ -1,30 +1,26 @@
-#!/usr/bin/env node
 const prerender = require('./lib');
-const puppeteer = require('puppeteer');
+const chromium = require('chrome-aws-lambda');
 
 (async () => {
-  // Use Puppeteer's built-in Chromium
-  const chromiumPath = puppeteer.executablePath();
+  const chromiumPath = await chromium.executablePath;
   process.env.CHROME_LOCATION = chromiumPath;
 
   const server = prerender();
 
-  // Optional middlewares
   server.use(prerender.sendPrerenderHeader());
   server.use(prerender.browserForceRestart());
-  // server.use(prerender.blockResources());
   server.use(prerender.addMetaTags());
   server.use(prerender.removeScriptTags());
   server.use(prerender.httpHeaders());
 
   server.start({
     chromeLocation: chromiumPath,
-    chromeFlags: [
+    chromeFlags: chromium.args.concat([
       '--headless',
       '--disable-gpu',
       '--no-sandbox',
       '--disable-dev-shm-usage',
       '--remote-debugging-port=9222',
-    ],
+    ]),
   });
 })();
